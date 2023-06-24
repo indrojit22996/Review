c5e76f118ab7e9d93d9d888b8aa3db9fd5dea1eb
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-06-24 19:24:53 +0530

    Add new things

---
+++ script.js
@@ -0,0 +1,36 @@
+// Object Deccler
+const reviews = [
+  {
+    id: 1,
+    name: 'Indrojit Bhaduri',
+    job: 'Web developer',
+    img: 'assate/Review-1.jpg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'Suvo Mandal',
+    job: 'Web dDsigner',
+    img: 'assate/Review-2.jpg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Tantu Mandal',
+    job: 'Data Science',
+    img: 'assate/Review-3.jpg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'Souvick Bhowmick',
+    job: 'The Boss',
+    img: 'assate/Review-4.jpg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+// Selecting Ealiments
+const img = document.querySelector('.author-img');
+const author = document.querySelector('.author-name');
+const job = document.querySelector('.author-job');
+const info = document.querySelector('.author-info');
