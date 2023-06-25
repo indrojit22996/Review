5403aab4375c0169d87f811f4b91e6bbf8cb55ab
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-06-25 09:17:34 +0530

    Update the logic

--- script.js
+++ script.js
@@ -2,35 +2,82 @@
 const reviews = [
   {
     id: 1,
-    name: 'Indrojit Bhaduri',
-    job: 'Web developer',
-    img: 'assate/Review-1.jpg',
+    name: "Indrojit Bhaduri",
+    job: "Web developer",
+    img: "assate/Review-1.jpg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
-    name: 'Suvo Mandal',
-    job: 'Web dDsigner',
-    img: 'assate/Review-2.jpg',
-    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    name: "Suvo Mandal",
+    job: "Web dDsigner",
+    img: "assate/Review-2.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
-    name: 'Tantu Mandal',
-    job: 'Data Science',
-    img: 'assate/Review-3.jpg',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    name: "Tantu Mandal",
+    job: "Data Science",
+    img: "assate/Review-3.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
-    name: 'Souvick Bhowmick',
-    job: 'The Boss',
-    img: 'assate/Review-4.jpg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    name: "Souvick Bhowmick",
+    job: "The Boss",
+    img: "assate/Review-4.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
 // Selecting Ealiments
-const img = document.querySelector('.author-img');
-const author = document.querySelector('.author-name');
-const job = document.querySelector('.author-job');
-const info = document.querySelector('.author-info');
+const img = document.querySelector(".author-img");
+const author = document.querySelector(".author-name");
+const job = document.querySelector(".author-job");
+const info = document.querySelector(".author-info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+// load initial item
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click', function () {
+  console.log('hello');
+
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
